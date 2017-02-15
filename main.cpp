faf10526918e68bb42971b62e9a0e998a30c35d4
Adrien Stadler <adrien-stadler@hotmail.fr>
2017-02-15 17:47:53 +0100

    Ajout des classes héritées d'individu, et du main

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//==============================
+//    INCLUDES
+//==============================
+
+#include <cstdio>
+#include <cstdlib>
+
+#include "CellA.h"
+#include "CellB.h"
+#include "Individu.h"
+#include "Environnement.h"
+
+//==============================
+//    FUNCTION DECLARATION
+//==============================
+
+//==============================
+//    MAIN
+//==============================
+
+int main(int argc, char const *argv[])
+{
+  return 0;
+}
