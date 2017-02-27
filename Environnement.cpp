b7750e3f235f75695cfda0b99ba3f21f71cab39c
Alexis Pister <alexis.pister@hotmail.com>
2017-02-27 17:15:19 +0100

    Constructeurs et destructeur

--- Environnement.cpp
+++ Environnement.cpp
@@ -13,19 +13,27 @@
 //==============================
 Environnement::Environnement()
 {
-	grille = new grille[32][32];
+	Case** grille = new Case*[32];
+	for(int i = 0; i<32; i++)
+		grille[i] = new Case[32];
 	fitness_min = 0.01;
 }
 
-Environnement::Environnement(const int x, const int y)
+Environnement::Environnement(const int x,const int y)
 {
-	grille = new grille[x][y];
+	Case** grille = new Case*[x];
+	for(int i = 0; i<x; i++)
+		grille[i] = new Case[y];
 	fitness_min= 0.01;
 }
 
 //==============================
 //    DESTRUCTOR
 //==============================
+
+Environnement::~Environnement(){
+	
+}
 
 
 //==============================
